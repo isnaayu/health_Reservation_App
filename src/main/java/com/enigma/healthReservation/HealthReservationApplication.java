91f41d2419e4875be9068ce6d1612331893ca509
isnaayu_m <isnaayu2@gmail.com>
2024-01-08 19:23:57 +0700

    Initial Version

---
+++ src/main/java/com/enigma/healthReservation/HealthReservationApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.healthReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigma/healthReservation/HealthReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigma.healthReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
